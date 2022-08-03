16c1e5036dd1e9a02a623534bcd43e5d07d23b46
KateGordin <kate.gordin@gmail.com>
2022-08-03 11:40:54 +0200

    Protect child routes with canActivateChild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,19 @@ import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 import { HomeComponent } from "./home/home.component";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'users', component: UsersComponent, children: [
       { path: ':id/:name', component: UserComponent },
     ] },
-    { path: 'servers', component: ServersComponent, children: [
+    { 
+      path: 'servers', 
+      // canActivate: [AuthGuard], 
+      canActivateChild: [AuthGuard],
+      component: ServersComponent, 
+      children: [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent },
     ] },
